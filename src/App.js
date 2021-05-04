6bac5f28888b640c1b4d23a361422431895465ad
amgeraldo14 <amgeraldo14@gmail.com>
2021-05-04 21:18:44 +0700

    fixing bugs, form login belum

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import { ChatEngine } from 'react-chat-engine'
+import ChatFeed from './components/ChatFeed'
+import './App.css'
 
 function App(){
   return (
-    <div>
-      test
-    </div>
+    <ChatEngine
+			height='100vh'
+			userName='amgeraldo14'
+			userSecret='admin1'
+			projectID='386dcf5c-d4aa-455b-bf44-d5134583b703'
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps}/>}
+		/>
   )
 }
 
